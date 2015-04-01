46ae8082b7013e4a5f05bde2c2da4c386b799b45
Kelly Andrews <kelly@kellyjandrews.com>
2015-04-01 12:29:18 -0400

    update app.jsx for bootstrap/jquery

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,9 @@ import TitleBar from './title-bar';
 import DataTable from './datatable';
 import Pagination from './pagination';
 
+import jQuery from 'jquery';
+global.jQuery = jQuery;
+import bootstrap from 'bootstrap';
 
 class DataGrid extends React.Component{
   render() {
